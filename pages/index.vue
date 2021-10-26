80ef79ca16f008b22ff472327c4e4a20aee4f541
Claudio Rescigno <cludso@yahoo.it>
2021-10-26 18:12:42 +0200

    charts added and working

--- pages/index.vue
+++ pages/index.vue
@@ -1,106 +1,33 @@
 <template>
-  <section>
-    <div class="chart">
-      <BarChart :data="barChartData" :options="barChartOptions" :height="400" />
-    </div>
-  </section>
+  <div>
+    <v-row>
+      <v-col cols="12" sm="5" md="3">
+        <bar-chart-component />
+      </v-col>
+      <v-col cols="12" sm="7" md="9">
+        <div>
+          <h2>Lorem ipsum</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+            voluptate laudantium itaque quos tempore necessitatibus pariatur,
+            quas atque porro cumque facere reiciendis non est rem laborum
+            asperiores molestias minus. Optio tempora laudantium animi nobis
+            quod delectus incidunt nisi. Iste, ut temporibus animi veritatis
+            expedita vel provident. Recusandae quos inventore in.
+          </p>
+        </div>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
-import BarChart from "~/components/BarChart.vue";
+import BarChartComponent from '~/components/BarChartComponent.vue'
 export default {
-  components: { BarChart },
-  data() {
-    return {
-      barChartData: {
-        labels: [
-          "2019-06",
-          "2019-07",
-          "2019-08",
-          "2019-09",
-          "2019-10",
-          "2019-11",
-          "2019-12",
-          "2020-01",
-          "2020-02",
-          "2020-03",
-        ],
-        datasets: [
-          {
-            label: "Visualizaciones",
-            data: [2, 1, 16, 3, 4, 5, 0, 0, 4, 12, 2],
-            backgroundColor: "rgba(20, 255, 0, 0.3)",
-            borderColor: "rgba(100, 255, 0, 1)",
-            borderWidth: 2,
-          },
-        ],
-      },
-      barChartOptions: {
-        responsive: true,
-        legend: {
-          display: false,
-        },
-        title: {
-          display: true,
-          text: "Google analytics data",
-          fontSize: 24,
-          fontColor: "#6b7280",
-        },
-        tooltips: {
-          backgroundColor: "#17BF62",
-        },
-        scales: {
-          xAxes: [
-            {
-              gridLines: {
-                display: true,
-              },
-            },
-          ],
-          yAxes: [
-            {
-              ticks: {
-                beginAtZero: true,
-                max: 7,
-                min: 0,
-                stepSize: 1,
-              },
-              gridLines: {
-                display: true,
-              },
-            },
-          ],
-        },
-      },
-    };
-  },
-};
+  components: { BarChartComponent },
+
+}
 </script>
 
-<style scoped>
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 400;
-  font-size: 100px;
-  color: #2e495e;
-  letter-spacing: 1px;
-  font-size: 6em;
-}
-.green {
-  color: #00c48d;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 1em;
-  color: #2e495e;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+<style>
 </style>
